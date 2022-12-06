f6129ecaaf1fe4eb0bfd4d2ea76ab6c074c1dc85
vtfeitosa <vfmidia@hotmail.com>
2022-12-06 00:58:02 -0300

    add atv 17 - q.1 - a,b,c

---
+++ Modulo_17/js/index.js
@@ -0,0 +1,98 @@
+"use strict"
+
+//Elemento do DOM no qual o conteúdo será impresso
+const contentBox = document.querySelector("#pageMainContent");
+
+//Capturando elementos do DOM para "imprimir" o header e footer
+const headerBox = document.querySelector("#developmentBox");
+const footerBox = document.querySelector("#infoBox");
+
+//Header
+headerBox .innerHTML = (`
+<p>Developed by <a href="https://github.com/vtfeitosa" target="_blank">vtfeitosa</a></p>
+`);
+
+//Footer
+footerBox.innerHTML = (`
+<p>So this page was developed as an activity of WEB Development Trail at Alpha EdTech, by the student <a href="https://www.linkedin.com/in/vtfeitosa/" target="_blank">Vitória Feitosa</a>.<br/> All the code is being studied, patience... Thank you for understanding. :) </p>
+<p>Last Update on December | 05 | 2022 </p>
+`);
+
+////////////////////////////
+
+const title = document.createElement("h1");
+const cep = document.createElement("input");
+const btn = document.createElement("button");
+const inpBox = document.createElement("div");
+const addressBox = document.createElement("div");
+const paragraph = document.createElement("p");
+
+function printPage(){
+
+    contentBox.appendChild(title);
+    contentBox.appendChild(inpBox);
+    inpBox.appendChild(cep);
+    inpBox.appendChild(btn);
+
+    cep.id = "cep";
+    btn.id = "btnConsult";
+    addressBox.id = "address";
+
+    title.innerText = "Digite um CEP:";
+    btn.innerText = "Consultar"
+
+}
+
+printPage();
+
+cep.addEventListener ('input', inputCep);
+cep.addEventListener ('keyup', keyupCep);
+
+
+// Função dedicada a limitar o input apenas para números
+function inputCep() {
+
+    if (isNaN(cep.value)){
+        cep.value = cep.value.slice(0, -1);
+
+    }
+}
+
+
+// Função dedicada a limitar o input com número 'x' de caracteres
+function keyupCep(){
+
+    if(cep.value.length>=9){
+        let cont = parseInt( cep.value.length - (cep.value.length - 8));
+        cep.value =  cep.value.slice(0, 8);
+        return;
+
+    }
+
+}
+
+
+// Função dedicada a capturar o input 'cep' e preencher o <p> com o Endereço completo.
+
+btnConsult.addEventListener('click',checkAdress);
+
+function checkAdress(){
+
+    if (cep.value.length < 8){
+        alert("Insira um CEP válido de 8 dígitos")
+    }
+
+    contentBox.appendChild(addressBox);
+    addressBox.appendChild(paragraph);
+
+    fetch(`https://cep.awesomeapi.com.br/json/${cep.value}`)
+
+        .then((res) => {
+            return res.json();
+        })
+
+        .then((data) => {
+            
+            paragraph.innerText = `${data.address}, ${data.district}, ${data.city} - ${data.state} (Latitude: ${data.lat} - Longitude: ${data.lng})`
+        })
+}
