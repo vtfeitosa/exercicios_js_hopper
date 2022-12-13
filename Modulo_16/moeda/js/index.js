1e4f70bc8caefcd531205ed17fe3f06ffdff7c9f
vtfeitosa <vfmidia@hotmail.com>
2022-12-12 21:11:16 -0300

    add atv 16 - base

---
+++ Modulo_16/moeda/js/index.js
@@ -0,0 +1,31 @@
+"use strict"
+
+//Elemento do DOM no qual o conteúdo será impresso
+const contentBox = document.querySelector("#pageMainContent");
+
+//Função para pular linha
+function skipLine(){
+    contentBox.innerHTML += ("</br>");
+}
+
+//Função para escrever um determinado conteúdo na página
+function writePageContent(content){
+    contentBox.innerHTML += (content);
+}
+
+//Capturando elementos do DOM para "imprimir" o header e footer
+const headerBox = document.querySelector("#developmentBox");
+const footerBox = document.querySelector("#infoBox");
+
+//Header
+headerBox .innerHTML = (`
+<p>Developed by <a href="https://github.com/vtfeitosa" target="_blank">vtfeitosa</a></p>
+`);
+
+//Footer
+footerBox.innerHTML = (`
+<p>So this page was developed as an activity of WEB Development Trail at Alpha EdTech, by the student <a href="https://www.linkedin.com/in/vtfeitosa/" target="_blank">Vitória Feitosa</a>.<br/> All the code is being studied, patience... Thank you for understanding. :) </p>
+<p>Last Update on monthName | dayNumber | 2022 </p>
+`);
+
+////////////////////////////
